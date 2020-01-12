cc18cdc0832a65b0a3e279e5b78586dc2c2e47ac
Ivan Glushko <ivanglushkodev@yandex.ru>
2020-01-12 21:58:46 +0200

    Added SwiftGen && removed remote github link

---
+++ Generated/Graphics/assets-images.swift
@@ -0,0 +1,9 @@
+//
+//  assets-images.swift
+//  SwiftProject-Staging
+//
+//  Created by Ivan Glushko on 12.01.2020.
+//  Copyright © 2020 Itch Design AS. All rights reserved.
+//
+
+import Foundation
